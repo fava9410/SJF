1e2d429693c0652f1d5f9542dac33fcde99a4c82
fava9410 <xxalexvargas@gmail.com>
2016-04-08 22:42:47 -0500

    mejor separado

--- js/logica.js
+++ js/logica.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-  $("#parar").hide();
+  $("#agregar").hide();
   var nombresProcesos = ["Nombres","A","B","C","D","E","F","G","H","I","J","K","L"];
   var tiemposRafaga = ["Tiempo Rafaga"];  
   var procesos = ["Proceso"];
@@ -11,60 +11,48 @@ $(document).ready(function(){
   var nomcolores = ["red","Crimson","blue","green","brown","yellow","purple","magenta","gray","Coral","DarkGreen"];
   var colores = ["red"];
   var alea = 0, intervalo = 10, tamaño = 20, contador = 1;
-  var bloqueados = ["Tiempo Bloqueado"];
-  var matriz = [procesos,llegada,tiemposRafaga,tiempoComienzo,tiempoFinalizacion,tiempoRetorno,tiempoEspera, bloqueados];
+  var matriz = [procesos,llegada,tiemposRafaga,tiempoComienzo,tiempoFinalizacion,tiempoRetorno,tiempoEspera];
 
   $("#inicio").click(function(){   
-	numeroProcesos();
+	  numeroProcesos();
+    rafaga();
+    quick_sort(tiemposRafaga);
     rellenarTabla();
     dibujarTabla();
     $("#inicio").hide();
-	$("#parar").show();
+	  $("#agregar").show();
     pintar();
 		pintar_numeros();
     setInterval(pintar_procesos,1000);                
   });  
   //$("#lienzo").css({"background-color":"black"});
 
-  $("#parar").click(function(){
-    var queda = 0;
-    for(var i=1; i<procesos.length; i++){
-      if(contador>=tiempoComienzo[i] && contador<tiempoFinalizacion[i]){ 
-        queda = tiempoFinalizacion[i]-contador+1;
-				bloqueados[i] = Math.round(Math.random()*7+1);
-        for(var j=1; j<procesos.length; j++){
-          llegada[j] = llegada[j]-queda;
-        }
-	procesos.push(procesos[i]);
-	llegada.push(contador+bloqueados[i]-2);
-	colores.push(colores[colores.length-1]);
-	colores[colores.length-2] = colores[i];
-	bloqueados.push(bloqueados[i]);
-	tiemposRafaga.push(queda);
-      }
-    }  
-      //borrarTabla();
-      rellenarTabla();      
-      dibujarTabla();		
-			pintar();				
+  $("#agregar").click(function(){
   });
   function numeroProcesos(){
-	  var numero = Math.round(Math.random()*6+1);	  
+	  var numero = Math.round(Math.random()*6+3);	  
 	  for(var i=1; i<=numero; i++){
 		  procesos.push(nombresProcesos[i]);
 		  colores.push(nomcolores[i]);
 	  }
 	  colores.push("white");
   }
-  function rellenarTabla(){	  	
-    //RAFAGA
+  function rafaga(){
     for(var i=0; i<procesos.length-1; i++){
-	  if(contador == 1){
-		alea = Math.round(Math.random()*6+1);
+      if(contador == 1){
+        alea = Math.round(Math.random()*6+1);
         tiemposRafaga.push(alea);
-	  }
-
+      }
     }
+  }
+  function rellenarTabla(){	  	
+    /*RAFAGA
+    for(var i=0; i<procesos.length-1; i++){
+  	  if(contador == 1){
+  		  alea = Math.round(Math.random()*6+1);
+        tiemposRafaga.push(alea);
+  	  }
+    }*/
     //LLEGADA
     for(var i=0; i< (procesos.length-1)*4; i+=4){
       if(contador == 1){
@@ -72,29 +60,14 @@ $(document).ready(function(){
         llegada.push(alea);  
       }      
     }
-		for(var i=1; i<procesos.length; i++){
-			if(contador == 1){
-				bloqueados[i] = 0;
-			}		
-		}
-    //FINALIZACION
     for(var i=1; i<procesos.length; i++){
       if(llegada[i]>=tiempoFinalizacion[i-1] || i==1){
         tiempoFinalizacion[i] = llegada[i]+tiemposRafaga[i];
       } else{
         tiempoFinalizacion[i] = tiempoFinalizacion[i-1]+tiemposRafaga[i];  
       }
-    }    
-    //RETORNO
-    for(var i=1; i<procesos.length; i++){
       tiempoRetorno[i] = tiempoFinalizacion[i]-llegada[i];
-    }
-    //ESPERA
-    for(var i=1; i<procesos.length; i++){
       tiempoEspera[i] = tiempoRetorno[i]-tiemposRafaga[i];
-    }
-    //COMIENZO
-    for(var i=1; i<procesos.length; i++){
       tiempoComienzo[i] = llegada[i]+tiempoEspera[i];
     }
   }
@@ -143,7 +116,7 @@ $(document).ready(function(){
       lienzo.font = "20px Arial";
       lienzo.fillText(i, (i+1)*(tamaño+intervalo)+10, procesos.length*(tamaño+intervalo)+100);
     }
-}
+  }
   function pintar_procesos(){		
     var elemento = document.getElementById("lienzo");
     var lienzo = elemento.getContext('2d');      
